78f0492182fc7ffae02a029d528790800f1232f9
Juuso Korhonen <juseek@gmail.com>
2019-08-13 14:53:42 +0300

    refactor(import): Update blob target filename.
    Hardcode the blob filename `tiles.mbtiles` to match the currently used path and naming convention.

--- run.sh
+++ run.sh
@@ -50,7 +50,7 @@ if [ -z "$AZURE_BLOB_SAS_ACCESS_KEY" ]; then
     exit 1
 fi
 
-URL="https://"$AZURE_STORAGE_ACCOUNT".blob.core.windows.net/"$CONTAINER_NAME"/"$FILENAME
+URL="https://"$AZURE_STORAGE_ACCOUNT".blob.core.windows.net/"$CONTAINER_NAME"/tiles.mbtiles"
 URL_WITH_SAS=$URL"?"$AZURE_BLOB_SAS_ACCESS_KEY
 echo "Uploading... to $URL"
 azcopy copy $FILENAME $URL_WITH_SAS
